1ef92c6495d38b4b3e28138e2e499b4156a01732
manya7 <70089047+manya7@users.noreply.github.com>
2026-02-16 14:12:01 +0530

    Fix syntax error in list definition

--- StepperMotor-FullStep.py
+++ StepperMotor-FullStep.py
@@ -5,7 +5,7 @@ step1 = Pin(18,Pin.OUT)
 step2 = Pin(19,Pin.OUT)
 step3 = Pin(22,Pin.OUT)
 step4 = Pin(23,Pin.OUT)
-list=[[1,1,0,0],[0,1,1,0],[0,0,1,1],[1,0,0,1]
+list=[[1,1,0,0],[0,1,1,0],[0,0,1,1],[1,0,0,1]]
 
 while True:
     for i in list:
