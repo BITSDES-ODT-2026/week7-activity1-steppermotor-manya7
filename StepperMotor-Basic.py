e5af29edc82356846b27c7e7139765af9f1c8417
manya7 <70089047+manya7@users.noreply.github.com>
2026-02-16 14:07:33 +0530

    Implement stepper motor control without loops
    Added code to control the stepper motor using GPIO pins.

--- StepperMotor-Basic.py
+++ StepperMotor-Basic.py
@@ -1,1 +1,32 @@
-#Write your code here to run the stepper motor without using any loop
+from machine import Pin,
+import time
+
+step1 = Pin(18,Pin.OUT)
+step2 = Pin(19,Pin.OUT)
+step3 = Pin(22,Pin.OUT)
+step4 = Pin(23,Pin.OUT)
+
+while True:
+    step1.value(1)
+    step2.value(1)
+    step3.value(0)
+    step4.value(0)
+    time.sleep_ms(3)
+
+    step1.value(0)
+    step2.value(1)
+    step3.value(0)
+    step4.value(0)
+    time.sleep_ms(3)
+    
+    step1.value(0)
+    step2.value(1)
+    step3.value(0)
+    step4.value(0)
+    time.sleep_ms(3)
+
+    step1.value(0)
+    step2.value(1)
+    step3.value(0)
+    step4.value(0)
+    time.sleep_ms(3)
