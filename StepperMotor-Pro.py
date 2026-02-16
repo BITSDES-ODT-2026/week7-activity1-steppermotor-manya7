145e947a540cac3e5bdbc7d5dadeb21c411c402a
manya7 <70089047+manya7@users.noreply.github.com>
2026-02-16 14:13:02 +0530

    Update StepperMotor-Pro.py

--- StepperMotor-Pro.py
+++ StepperMotor-Pro.py
@@ -1,1 +1,25 @@
+from machine import Pin,
+import time
+
+step1 = Pin(18,Pin.OUT)
+step2 = Pin(19,Pin.OUT)
+step3 = Pin(22,Pin.OUT)
+step4 = Pin(23,Pin.OUT)
+list=[[1,1,0,0],[0,1,1,0],[0,0,1,1],[1,0,0,1]]
+list2=[[1,0,0,1],[0,0,1,1],[0,1,1,0],[1,1,0,0]]
+
+while True:
+    for i in list:
+        step1.value(i[0])
+        step2.value(i[1])
+        step3.value(i[2])
+        step4.value(i[3])
+        time.sleep_ms(5)
+       for i in list2:
+        step1.value(i[0])
+        step2.value(i[1])
+        step3.value(i[2])
+        step4.value(i[3])
+        time.sleep_ms(5)
+
 
